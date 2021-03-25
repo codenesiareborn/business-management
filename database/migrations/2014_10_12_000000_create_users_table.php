83d411eceeb82ba5b7a80bf56999d2879cb15a3d
bagas5ri <bagastitik@gmail.com>
2021-03-26 01:10:36 +0700

    Update Schema(users): +phone; Create Seeder(users); Update Registration Backend

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone', 30);
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
